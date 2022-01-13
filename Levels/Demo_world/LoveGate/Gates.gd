66e1a59d161962de12354c4e1aaa8b3333d251c6
Lemonbrush <aa_rubtsov@mail.ru>
2022-01-13 23:42:05 +0300

    add decoration elements
    add cut scene

--- Levels/Demo_world/LoveGate/Gates.gd
+++ Levels/Demo_world/LoveGate/Gates.gd
@@ -1,7 +1,22 @@
 extends Node2D
 
+onready var area2d = $Area2D
+
+var isAbleToTransition = false
+
 func _ready():
-	pass
+	area2d.connect("body_entered", self, "on_player_entered")
+	area2d.connect("body_exited", self, "on_player_exited")
 		
+func _input(event):
+	if event.is_action_pressed("jump") && isAbleToTransition:
+		get_tree().change_scene("res://Levels/Demo_world/DemoWorld.tscn")
+
 func on_open_gates_call():
 	$AnimationPlayer.play("Open")
+
+func on_player_entered(_body):
+	isAbleToTransition = true
+	
+func on_player_exited(_body):
+	isAbleToTransition = false 
