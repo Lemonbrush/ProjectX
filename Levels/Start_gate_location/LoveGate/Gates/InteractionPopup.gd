388e173975772a91b619bef411b0f7f0cc816ad1
Lemonbrush <aa_rubtsov@mail.ru>
2022-05-23 19:42:15 +0300

    add popup animation
    correct door entering logic

--- Levels/Start_gate_location/LoveGate/Gates/InteractionPopup.gd
+++ Levels/Start_gate_location/LoveGate/Gates/InteractionPopup.gd
@@ -2,23 +2,34 @@ extends Node2D
 
 export(NodePath) var interaction_controller_path
 
-onready var label = $MarginNode/CenterContainer/Label
+onready var marginNode = $MarginNode
+onready var label = $MarginNode/CenterContainer/HBoxContainer/Label
+onready var tween = $Tween
 
 export(String) var labelText = ""
 
 func _ready():
+	modulate.a = 0.0
 	label.text = labelText
-	label.visible = false
 	var interaction_controller = get_node(interaction_controller_path) 
 	interaction_controller.connect("on_approach", self, "_on_approach")
 	interaction_controller.connect("on_leave", self, "_on_leave")
 	interaction_controller.connect("on_interact", self, "_on_interact")
 	
 func show():
-	label.visible = true
+	if modulate.a != 1.0:
+		marginNode.position.y = 10
+		tween.stop(self)
+		tween.interpolate_property(self, 'modulate:a', get_modulate().a, 1.0, 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0)
+		tween.interpolate_property(marginNode, 'position:y', marginNode.position.y, 0.0, 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0)
+		tween.start()
 
 func hide():
-	label.visible = false
+	if modulate.a != 0.0:
+		tween.stop(self)
+		tween.interpolate_property(self, 'modulate:a', get_modulate().a, 0.0, 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0)
+		tween.interpolate_property(marginNode, 'position:y', marginNode.position.y, -10, 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0)
+		tween.start()
 	
 func _on_approach():
 	show()
