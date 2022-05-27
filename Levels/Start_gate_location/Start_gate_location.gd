82eea5d38d9256d19409f0139cdcd578c6cadcfd
Lemonbrush <aa_rubtsov@mail.ru>
2022-05-27 19:34:27 +0300

    add picked item to picking item animation

--- Levels/Start_gate_location/Start_gate_location.gd
+++ Levels/Start_gate_location/Start_gate_location.gd
@@ -5,7 +5,7 @@ onready var gates 	= $Gates
 func _ready():
 	var page_collectable = find_node("Page")
 	if page_collectable:
-		page_collectable.connect("page_collected", self, "on_page_collected") 
+		page_collectable.connect("item_collected", self, "on_page_collected") 
 	
 func on_page_collected():
 	animationPlayer.queue("Gate_opening_cut_scene")
--- Player/Animation_scenes/Item_picking_player.gd
+++ Player/Animation_scenes/Item_picking_player.gd
@@ -2,6 +2,15 @@ extends Node2D
 
 signal animationFinished
 
+onready var itemPosition = $ItemPosition
+
+var itemScene
+
+func _ready():
+	var item_instance = itemScene.instance()
+	add_child(item_instance)
+	item_instance.global_position = itemPosition.global_position
+
 func finish_animation():
 	emit_signal("animationFinished")
 	queue_free()
