b158273eab3e5c75f9bd4947300ee3b8ed88da43
Lemonbrush <aa_rubtsov@mail.ru>
2022-05-28 19:45:39 +0300

    fix barrel crash logic

--- WorldObjects/Destructables/Barrel/Still_barrel/Barrel.gd
+++ WorldObjects/Destructables/Barrel/Still_barrel/Barrel.gd
@@ -6,14 +6,27 @@ onready var hazard_area 			= $HazardArea
 
 var velocity 					= Vector2.ZERO
 
+onready var ground_ray1 			= $GroundRay1
+onready var ground_ray2 			= $GroundRay2
+onready var ground_ray3 			= $GroundRay3
+onready var ray_array			= [ground_ray1, ground_ray2, ground_ray3]
+
 func _ready():
+	
 	hazard_area.connect("area_entered", self, "destruct")
 	
 func _process(delta):
 	velocity.y += 600 * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
+	check_ground()
+	
+func check_ground():
+	for ray in ray_array:
+		ray.force_raycast_update()
+		if ray.is_colliding():
+			destruct()
 
-func destruct(_area):
+func destruct(_area = null):
 	var barrel_crush_instance = barrel_crush_scene.instance()
 	get_parent().add_child_below_node(self, barrel_crush_instance)
 	barrel_crush_instance.global_position = global_position
