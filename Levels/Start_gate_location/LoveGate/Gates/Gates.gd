063da88dd6bd9aee4f3216265d5c5955d0cfebf1
Alexander <aa_rubtsov@mail.ru>
2022-09-13 19:55:17 +0300

    fix start gate interaction popup

--- Levels/Start_gate_location/LoveGate/Gates/Gates.gd
+++ Levels/Start_gate_location/LoveGate/Gates/Gates.gd
@@ -3,8 +3,8 @@ extends Node2D
 export(String, FILE, "*.tscn, *scn") var nextScenePath
 export(String) var nextDoorName = "-"
 
-onready var area2d = $Area2D
 onready var interactionController = $InteractionController
+onready var interactionPopup = $InteractionPopup
 onready var animationPlayer = $AnimationPlayer
 onready var gateSign = $GatesText
 
@@ -12,8 +12,8 @@ export var isAbleToTransition = false
 
 func _ready():
 	interactionController.disabled(true)
-	area2d.connect("body_entered", self, "on_player_entered")
-	area2d.connect("body_exited", self, "on_player_exited")
+	interactionController.connect("on_approach", self, "on_player_entered")
+	interactionController.connect("on_leave", self, "on_player_exited")
 	
 	interactionController.connect("on_interact", self, "_on_interact")
 	
@@ -32,12 +32,16 @@ func set_gate_opened():
 	
 # Area2D functions
 
-func on_player_entered(_body):
+func on_player_entered(body):
+	if "is_entering_out" in body && body.is_entering_out == false:
+		interactionPopup.show()
 	isAbleToTransition = true
 	
-func on_player_exited(_body):
+func on_player_exited():
+	interactionPopup.hide()
 	isAbleToTransition = false
 
 func _on_interact(_body):
+	interactionPopup.hide()
 	Global.door_name = nextDoorName
 	EventBus.player_entered_door(nextScenePath)
