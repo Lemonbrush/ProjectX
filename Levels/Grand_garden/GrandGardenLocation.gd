8f539f31273847e83065312b10255ec871dfbb6a
Alexander <aa_rubtsov@mail.ru>
2022-11-05 19:24:44 +0200

    correct level persistence

--- Levels/Grand_garden/GrandGardenLocation.gd
+++ Levels/Grand_garden/GrandGardenLocation.gd
@@ -5,6 +5,8 @@ onready var water_level = $WaterSurface
 onready var animation_player = $AnimationPlayer
 onready var left_water_tower = $World_objects/Water_tower_1
 onready var right_water_tower = $World_objects/Water_tower_2
+onready var left_fountains = $Fountains/Left_tower_activated_fountains
+onready var right_fountains = $Fountains/Right_tower_activated_fountains
 
 var water_surface_final_phase = 566
 var water_surface_second_phase = 678
@@ -16,11 +18,22 @@ func _ready():
 	left_water_tower.connect("cork_destroyed_on_water_tower", self, "did_destroy_cork_on_water_tower")
 	right_water_tower.connect("cork_destroyed_on_water_tower", self, "did_destroy_cork_on_water_tower")
 	
-	EventBus.connect("show_white_flower_grow_cut_scene", self, "show_white_flower_grow_cut_scene")
+	var _connection = EventBus.connect("show_white_flower_grow_cut_scene", self, "show_white_flower_grow_cut_scene")
 	
 	match GameEventConstants.get_constant("grand_garden_water_level"):
 		1: current_water_surface = water_surface_second_phase
 		2: current_water_surface = water_surface_final_phase
+	
+	var active_tower_fountains = GameEventConstants.get_constant("grand_garden_water_level")
+	left_fountains.visible = false
+	right_fountains.visible = false
+	
+	if active_tower_fountains == 1.0:
+		right_fountains.visible = true
+	elif active_tower_fountains == 2.0:
+		right_fountains.visible = true
+		left_fountains.visible = true
+		
 
 func did_destroy_cork_on_water_tower(tower_number):
 	match tower_number:
@@ -28,7 +41,6 @@ func did_destroy_cork_on_water_tower(tower_number):
 		1: animation_player.play("right_tower_cork_animation")
 
 func rise_water_level():
-	var next_water_surface_level
 	match current_water_surface:
 		water_surface_start_phase: 
 			current_water_surface = water_surface_second_phase
