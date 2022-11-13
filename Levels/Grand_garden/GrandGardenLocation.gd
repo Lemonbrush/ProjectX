c6eaffd4b009a540a7b18b4932a12df1d651cfbd
Alexander <aa_rubtsov@mail.ru>
2022-11-13 18:18:03 +0200

    add young flower

--- Levels/Grand_garden/GrandGardenLocation.gd
+++ Levels/Grand_garden/GrandGardenLocation.gd
@@ -8,6 +8,7 @@ onready var right_water_tower = $World_objects/Water_tower_2
 onready var left_fountains = $Fountains/Left_tower_activated_fountains
 onready var right_fountains = $Fountains/Right_tower_activated_fountains
 onready var cactusLoveParticles = $Upper_background_objects/Dwellers/Cactus_flower_dweller/LoveParticles
+onready var young_flower_interaction_emitter = $Upper_background_objects/Young_flower/InteractionEmitterObject
 
 var water_surface_final_phase = 566
 var water_surface_second_phase = 678
@@ -16,11 +17,13 @@ var water_surface_start_phase = 873
 var current_water_surface = water_surface_start_phase
 
 func _ready():
-	left_water_tower.connect("cork_destroyed_on_water_tower", self, "did_destroy_cork_on_water_tower")
-	right_water_tower.connect("cork_destroyed_on_water_tower", self, "did_destroy_cork_on_water_tower")
+	var _left_water_tower_connection = left_water_tower.connect("cork_destroyed_on_water_tower", self, "did_destroy_cork_on_water_tower")
+	var _right_water_tower_connection = right_water_tower.connect("cork_destroyed_on_water_tower", self, "did_destroy_cork_on_water_tower")
 	
-	var _connection = EventBus.connect("show_white_flower_grow_cut_scene", self, "show_white_flower_grow_cut_scene")
+	var _show_white_flower_grow_cut_scene_connection = EventBus.connect("show_white_flower_grow_cut_scene", self, "show_white_flower_grow_cut_scene")
 	var _cactus_particles_connection = EventBus.connect("show_cuctus_love_particles", self, "show_cuctus_love_particles")
+	
+	var _young_flower_interaction_emitter_connection = young_flower_interaction_emitter.connect("interacted_with_arg", self, "did_interact_with_arg")
 	
 	match GameEventConstants.get_constant("grand_garden_water_level"):
 		1: current_water_surface = water_surface_second_phase
@@ -36,6 +39,8 @@ func _ready():
 		right_fountains.visible = true
 		left_fountains.visible = true
 		
+func did_interact_with_arg(arg):
+	animationPlayer.play("Grand_flower_opening_cut_scene")
 
 func did_destroy_cork_on_water_tower(tower_number):
 	match tower_number:
