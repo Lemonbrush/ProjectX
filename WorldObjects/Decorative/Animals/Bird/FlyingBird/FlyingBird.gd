37203474d27c15df6431bffdd76b975f9fa8c7da
Alexander <aa_rubtsov@mail.ru>
2022-11-19 20:06:18 +0300

    clean project folders

--- WorldObjects/Decorative/Animals/Bird/FlyingBird/FlyingBird.gd
+++ WorldObjects/Decorative/Animals/Bird/FlyingBird/FlyingBird.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 onready var flyTween = $Tween
+onready var alphaTween = $TweenAlpha
 onready var visibilityNotifier = $VisibilityNotifier2D
 
 func _ready():
@@ -10,8 +11,13 @@ func _ready():
 	var fly_destination = Vector2(destination_x, position.y - 500)
 	scale.x = sign(destination_x)
 	
-	flyTween.interpolate_property(self, "position", position, fly_destination, 15, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	var animation_time = 15
+	
+	flyTween.interpolate_property(self, "position", position, fly_destination, animation_time, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
 	flyTween.start()
+	
+	alphaTween.interpolate_property(self, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), animation_time, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	alphaTween.start()
 
 func screen_exited():
 	queue_free()
