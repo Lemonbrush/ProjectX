3c365cb42e6fc6795019e311058f4d41d39a8a27
Alexander <aa_rubtsov@mail.ru>
2022-12-02 00:22:49 +0300

    add guardian island camera focus area

--- Levels/Harbor_house/GuardianIsland.gd
+++ Levels/Harbor_house/GuardianIsland.gd
@@ -3,6 +3,7 @@ extends Node2D
 onready var tween = $IslandLiftTween
 onready var guardianStatue = $Background_world_objects/Guardian_statue
 onready var liftWaitTimer = $LiftWaitTimer
+onready var cameraFocusArea = $CameraFocusArea2D
 
 var topPosition = Vector2(103, -724)
 var middlePosition = Vector2(103, -180)
@@ -12,6 +13,8 @@ func _ready():
 	var _lift_up_connection = guardianStatue.connect("lift_island_with_direction_up", self, "lift_island")
 	var _lift_wait_timer_connection = liftWaitTimer.connect("timeout", self, "lift_island_down")
 	var _tween_connection = tween.connect("tween_completed", self, "start_wait_timer_if_needed")
+	var _camera_focus_area_enter_connection = cameraFocusArea.connect("body_entered", self, "camera_focus_area_entered")
+	var _camera_focus_area_exit_connection = cameraFocusArea.connect("body_exited", self, "camera_focus_area_exited")
 	
 	if GameEventConstants.get_constant("guardian_statue_satisfied"):
 		liftWaitTimer.start()
@@ -32,3 +35,9 @@ func lift_island_down():
 	if position != bottomPosition:
 		tween.interpolate_property(self, "position", position, bottomPosition, 15, Tween.TRANS_LINEAR, Tween.TRANS_LINEAR)
 		tween.start()
+
+func camera_focus_area_entered(_body):
+	EventBus.camera_focuse_animation(Vector2(1.5, 1.5), 1)
+
+func camera_focus_area_exited(_body):
+	EventBus.camera_focuse_animation(Vector2(1, 1), 1)
