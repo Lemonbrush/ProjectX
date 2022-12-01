cd663b4098c35bfd45d4f3971223eb07b741eb6f
Alexander <aa_rubtsov@mail.ru>
2022-12-02 00:06:49 +0300

    correct auto lift down mechanism

--- Levels/Harbor_house/GuardianIsland.gd
+++ Levels/Harbor_house/GuardianIsland.gd
@@ -12,14 +12,20 @@ func _ready():
 	var _lift_up_connection = guardianStatue.connect("lift_island_with_direction_up", self, "lift_island")
 	var _lift_wait_timer_connection = liftWaitTimer.connect("timeout", self, "lift_island_down")
 	var _tween_connection = tween.connect("tween_completed", self, "start_wait_timer_if_needed")
+	
+	if GameEventConstants.get_constant("guardian_statue_satisfied"):
+		liftWaitTimer.start()
 
 func lift_island(direction_up):
 	var destination_position = topPosition if direction_up else bottomPosition
-	tween.interpolate_property(self, "position", position, destination_position, 15, Tween.TRANS_LINEAR, Tween.TRANS_LINEAR)
-	tween.start()
+	
+	if destination_position != position && !tween.is_active():
+		tween.interpolate_property(self, "position", position, destination_position, 15, Tween.TRANS_LINEAR, Tween.TRANS_LINEAR)
+		tween.start()
 	
 func start_wait_timer_if_needed(_arg1, _arg2):
-	liftWaitTimer.start()
+	if position != bottomPosition:
+		liftWaitTimer.start()
 
 func lift_island_down():
 	liftWaitTimer.stop()
