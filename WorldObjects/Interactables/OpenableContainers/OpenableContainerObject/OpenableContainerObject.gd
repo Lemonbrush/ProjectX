8ff204caf2cf858e349510ffe1e2b06f014c259a
Alexander <aa_rubtsov@mail.ru>
2023-01-29 21:57:17 +0300

    fix chest popup

--- WorldObjects/Interactables/OpenableContainers/OpenableContainerObject/OpenableContainerObject.gd
+++ WorldObjects/Interactables/OpenableContainers/OpenableContainerObject/OpenableContainerObject.gd
@@ -9,6 +9,7 @@ onready var interactionPopup = $InteractionPopup
 func _ready():
 	interactionController.connect("on_interact", self, "_on_open_pressed") 
 	interactionController.connect("on_approach", self, "_on_player_enter")
+	interactionController.connect("on_leave", self, "_on_player_leave")
 
 func _on_open_pressed(_body):
 	var openAnimationSceneInstance = openAnimationScene.instance()
@@ -16,7 +17,11 @@ func _on_open_pressed(_body):
 	openAnimationSceneInstance.set_owner(get_tree().get_current_scene())
 	openAnimationSceneInstance.global_position = global_position
 	openAnimationSceneInstance.innerItemScene = innerItemScene
+	interactionPopup.hide()
 	queue_free()
 
 func _on_player_enter(_body):
 	interactionPopup.show()
+
+func _on_player_leave():
+	interactionPopup.hide()
