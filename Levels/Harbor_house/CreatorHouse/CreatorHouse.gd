168c664ed0e6e1beb795cccdbbc0bf37c7472097
Alexander <aa_rubtsov@mail.ru>
2023-02-04 22:13:49 +0300

    clean code

--- Levels/Harbor_house/CreatorHouse/CreatorHouse.gd
+++ Levels/Harbor_house/CreatorHouse/CreatorHouse.gd
@@ -3,7 +3,7 @@ extends BaseLevel
 onready var animation_player = $AnimationPlayer
 
 func _ready():
-	EventBus.connect("show_creator_house_desk_scene", self, "show_creator_house_desk_scene") 
+	var _desk_sign_connection = EventBus.connect("show_creator_house_desk_scene", self, "show_creator_house_desk_scene") 
 	
 func show_creator_house_desk_scene():
 	animationPlayer.play("creator_house_desk_scene")
