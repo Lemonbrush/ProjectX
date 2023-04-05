62ececbdf2088f6d61c6fcd8a9d34ca91c479910
Alexander <aa_rubtsov@mail.ru>
2023-04-06 01:56:02 +0300

    add particles cache

---
+++ Utils/Singletons/ParticlesCache.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+var wide_torch_fire = preload("res://Resources/Particles/Fire/Wide_torch_fire_process_material.tres")
+var crush_dust = preload("res://Resources/Particles/Crush_particles/Crush_dust/CrushDustProcessMaterial.tres")
+var crush_shine = preload("res://Resources/Particles/Crush_particles/Crush_shine/CrushShineProcessMaterial.tres")
+var crush_vase = preload("res://Resources/Particles/Crush_particles/Vase_crush/VaseCrushProcessMaterial.tres")
+
+var materials = [
+	wide_torch_fire,
+	crush_dust,
+	crush_shine,
+	crush_vase
+	]
+
+func _ready():
+	for material in materials:
+		var particle_instance = Particles2D.new()
+		particle_instance.set_process_material(material)
+		particle_instance.set_one_shot(true)
+		particle_instance.set_modulate(Color(1,1,1,0))
+		particle_instance.set_emitting(true)
+		add_child(particle_instance)
