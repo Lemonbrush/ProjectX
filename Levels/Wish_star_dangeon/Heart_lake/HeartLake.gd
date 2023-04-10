7a4d769d4eed88c330f21a181daf0fd43abdff6d
Alexander <aa_rubtsov@mail.ru>
2023-04-10 21:20:27 +0300

    add soul star sprite

--- Levels/Wish_star_dangeon/Heart_lake/HeartLake.gd
+++ Levels/Wish_star_dangeon/Heart_lake/HeartLake.gd
@@ -4,4 +4,4 @@ func _ready():
 	var _connection = EventBus.connect("show_heart_assembling_cutscene", self, "show_heart_assembling_cutscene") 
 	
 func show_heart_assembling_cutscene():
-	print("!!!!")
+	animationPlayer.play("Heart_assembling_cutscene")
