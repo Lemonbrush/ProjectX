94898d93a27da609b94c7dd4bc649dd66e79e3fb
Alexander <aa_rubtsov@mail.ru>
2023-06-10 00:05:50 +0300

    correct platform visibility

--- Levels/FinalLevel/FinalLevel.gd
+++ Levels/FinalLevel/FinalLevel.gd
@@ -1,6 +1,6 @@
 extends BaseLevel
 
-onready var platform_approachDetector = $Tower/Middle_world_objects/PlatformApproachDetector
+onready var platform_approachDetector = $PlatformApproachDetector
 
 func _ready():
 	var _elevator_connection = platform_approachDetector.connect("player_did_enter_elevator", self, "player_did_enter_elevator")
