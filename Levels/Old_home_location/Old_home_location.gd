224b68fea504479f7cbf9bdef5c8583b16d1770e
Alexander <aa_rubtsov@mail.ru>
2023-07-10 01:25:30 +0300

    correct me entering portal animation

--- Levels/Old_home_location/Old_home_location.gd
+++ Levels/Old_home_location/Old_home_location.gd
@@ -3,6 +3,14 @@ extends BaseLevel
 var playerScene = preload("res://Player/Player.tscn")
 
 onready var player_spawn_position = $World_objects/PlayerSpawnPosition
+onready var portal_interaction_object = $World_objects/PortalInteractionEmitter
+onready var sofa_entering_portal_animation_player = $AnimationPlayer/SofaEnteringPortalAnimationPlayer
+
+func _ready():
+	var _connection = portal_interaction_object.connect("interacted_with_arg", self, "player_interacted_with_portal")
+
+func player_interacted_with_portal(_args):
+	sofa_entering_portal_animation_player.play("Animate")
 
 func spawn_player():
 	var playerInstance = playerScene.instance()
