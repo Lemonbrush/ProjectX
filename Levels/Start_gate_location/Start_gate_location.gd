b82e0a4f664e070e811aa1af424e029e5b701c08
Alexander <aa_rubtsov@mail.ru>
2023-07-16 00:56:16 +0300

    correct game constant

--- Levels/Start_gate_location/Start_gate_location.gd
+++ Levels/Start_gate_location/Start_gate_location.gd
@@ -8,9 +8,9 @@ func _ready():
 	
 	if GameEventConstants.is_cauldron_quest_completed():
 		lonelyDweller.visible = false
-	
+
 	if GameEventConstants.get_constant("is_start_gate_first_entrance"):
-		GameEventConstants.set_constant("is_start_gate_first_entrance", true)
+		GameEventConstants.set_constant("is_start_gate_first_entrance", false)
 		animationPlayer.play("First_entrance_animation")
 	
 func _on_key_item_collected(item_name):
