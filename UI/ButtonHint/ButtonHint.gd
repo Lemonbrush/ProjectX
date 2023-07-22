48de1621e3345c1211c2f52a6b18fb874cd5d5d6
Alexander <aa_rubtsov@mail.ru>
2023-07-22 20:14:59 +0300

    Add adaptive button hint to dialog boxes

--- UI/ButtonHint/ButtonHint.gd
+++ UI/ButtonHint/ButtonHint.gd
@@ -1,8 +1,19 @@
 extends Control
 
-export (String) var text = ""
+export (String) var button_action = ""
 
 onready var label = $PanelContainer/MarginContainer/Label
 
 func _ready():
-	label.text = text
+	setup_hint()
+
+func set_hint_action(button_action):
+	self.button_action = button_action
+	setup_hint()
+
+func setup_hint():
+	var action_list = InputMap.get_action_list(button_action)
+	if action_list and action_list.size() > 0:
+		label.text = action_list[0].as_text()
+		return
+	label.text = "?"
