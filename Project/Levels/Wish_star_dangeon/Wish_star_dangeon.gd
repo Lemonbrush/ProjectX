06895f47a17d0fc6ff72da51b37adfcc8b545276
Alexander <aa_rubtsov@mail.ru>
2023-08-11 22:02:54 +0300

    correct wish star dangeon cutscene trigger condition

--- Project/Levels/Wish_star_dangeon/Wish_star_dangeon.gd
+++ Project/Levels/Wish_star_dangeon/Wish_star_dangeon.gd
@@ -1,10 +1,12 @@
 extends BaseLevel
 
+onready var cutscene_torch = $Cave_objects/Middle_world_objects/Moving_platforms/Interactive_platforms/Interactive_platform_1/Wide_torch
+
 func _ready():
 	var _success = EventBus.connect("player_picked_up_item", self, "_on_item_collected") 
 	
 func _on_item_collected(item_name):
-	if item_name == "LeftSideHeartItem":
+	if item_name == "LeftSideHeartItem" and !cutscene_torch.is_torch_activated:
 		animationPlayer.queue("First_torch_ignite_cutscene")
 		
 	if item_name == "RightSideHeartItem":
