05a47d4fb7a43cc99abe2f0e31ba84e93af5da44
Alexander <aa_rubtsov@mail.ru>
2023-08-19 19:46:10 +0300

    add dweller sound models

--- Project/WorldObjects/Dialog/DialogController/DialogController.gd
+++ Project/WorldObjects/Dialog/DialogController/DialogController.gd
@@ -2,15 +2,17 @@ extends Node2D
 
 export(String) var dialogId
 export(String) var interaction_popup_label_text = ""
+export (Resource) var voice_generator_configuration_file
+export(bool) var is_player_interaction_active = true
 
 onready var dialogTextBoxController = $DialogTextBoxController
 onready var interactionController = $InteractionController
 onready var interactionPopup = $InteractionPopup
-export(bool) var is_player_interaction_active = true
 
 func _ready():
 	interactionController.is_player_interaction_active = is_player_interaction_active
 	dialogTextBoxController.set_dialog_id(dialogId) 
+	dialogTextBoxController.set_letter_sounds_resource(voice_generator_configuration_file)
 	interactionPopup.set_label_text(interaction_popup_label_text)
 	
 	interactionController.connect("on_interact", self, "on_npc_interact")
