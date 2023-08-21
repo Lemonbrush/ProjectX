e79db71cfd1901e75a869d9e2796f0c289fe3e29
Alexander <aa_rubtsov@mail.ru>
2023-08-21 18:59:49 +0300

    add mill background sounds

--- Project/Levels/Central_hab/Mill/Shaft/Shaft.gd
+++ Project/Levels/Central_hab/Mill/Shaft/Shaft.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
 onready var animationPlayer = $AnimationPlayer
+onready var working_gears_audio_player = $Audio/WorkingGearAudioStreamPlayer
 
 func _ready():
 	if GameEventConstants.constants.has("is_mill_gear_clanged") &&  GameEventConstants.constants["is_mill_gear_clanged"]:
 		animationPlayer.play("Locked")
 	else:
-		animationPlayer.play("Run")
+		mill_gear_cleared()
 		
 	var _connection = EventBus.connect("mill_gear_barrel_destructed", self, "mill_gear_cleared")
 
 func mill_gear_cleared():
+	working_gears_audio_player.play()
 	animationPlayer.play("Run")
