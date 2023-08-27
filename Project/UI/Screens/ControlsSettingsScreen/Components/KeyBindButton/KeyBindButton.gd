949b28fbe2dbe539bc04657797793c8e43e9d5da
Alexander <aa_rubtsov@mail.ru>
2023-08-27 22:54:04 +0300

    add key bind logic

--- Project/UI/Screens/ControlsSettingsScreen/Components/KeyBindButton/KeyBindButton.gd
+++ Project/UI/Screens/ControlsSettingsScreen/Components/KeyBindButton/KeyBindButton.gd
@@ -1,10 +1,21 @@
 tool
-
 extends Control
 
+signal did_press_rebind_key(key_action)
+
 export (String) var button_text = ""
+export (String) var key_action_title = ""
 
 onready var button = $HBoxContainer/AnimatedDialogButton
+onready var button_hint = $HBoxContainer/ButtonHint
 
 func _ready():
+	_setup_ui()
+	button.connect("pressed_and_resolved", self, "did_press_button")
+
+func did_press_button():
+	emit_signal("did_press_rebind_key", key_action_title)
+
+func _setup_ui():
+	button_hint.set_hint_action(key_action_title)
 	button.text = button_text
--- Project/UI/Screens/ControlsSettingsScreen/ControlsSettingsScreen.gd
+++ Project/UI/Screens/ControlsSettingsScreen/ControlsSettingsScreen.gd
@@ -1,16 +1,25 @@
 extends CanvasLayer
 
+signal back_pressed()
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var exit_button = $MainMarginContainer/SettingsMarginContainer/ScrollContainer/VBoxContainer/VBoxContainer2/ExitButton
+onready var key_binds_container = $MainMarginContainer/SettingsMarginContainer/ScrollContainer/VBoxContainer/KeyButtonsContainer
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	exit_button.connect("pressed_and_resolved", self, "did_press_exit_button")
+	connect_key_bind_buttons()
+	setup_ui()
 
+func setup_ui():
+	exit_button.grab_focus_without_animation()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func did_press_exit_button():
+	queue_free()
+	emit_signal("back_pressed")
+
+func connect_key_bind_buttons():
+	for child in key_binds_container.get_children():
+		child.connect("did_press_rebind_key", self, "_did_press_rebind_key")
+
+func _did_press_rebind_key(key_action):
+	print(key_action)
