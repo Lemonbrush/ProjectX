cfe9099a6be56296d3903ba22f332d8de29ccef9
Alexander <aa_rubtsov@mail.ru>
2023-12-12 21:50:42 +0300

    add kissing spirit sounds

--- Project/Levels/Central_hab/World_objects/Lovers/Lovers.gd
+++ Project/Levels/Central_hab/World_objects/Lovers/Lovers.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+signal set_background_music_volume(newVolume)
+signal reset_background_music_volume()
+
 onready var loversParticles = $LoversHeartsParticles
 onready var animationPlayer = $AnimationPlayer
 
@@ -15,3 +18,9 @@ func configure_lovers_particles():
 func show_lovers_cut_scene():
 	loversParticles.emitting = true
 	animationPlayer.play("First_kiss_spirit_appear")
+
+func lower_music_volume():
+	emit_signal("set_background_music_volume", -20)
+
+func reset_background_music_volume():
+	emit_signal("reset_background_music_volume")
