f26b1acb502f3d81157404ed59bce305acfe083c
Alexander <aa_rubtsov@mail.ru>
2023-12-17 01:44:31 +0300

    add brush item dispatcher

--- Project/WorldObjects/Technical/ItemDispatcher/ItemDispatcher.gd
+++ Project/WorldObjects/Technical/ItemDispatcher/ItemDispatcher.gd
@@ -3,6 +3,8 @@ extends Node
 export(PackedScene) var itemScene
 
 func dispatch():
+	if !itemScene:
+		return
 	var item_instance = itemScene.instance()
 	if item_instance && item_instance.has_method("pick_up_item"):
 		item_instance.pick_up_item()
