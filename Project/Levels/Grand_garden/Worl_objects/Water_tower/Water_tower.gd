e7f9e59f3edf123b4af76e3580d7791b7015f684
Alexander <aa_rubtsov@mail.ru>
2023-12-19 22:55:03 +0300

    fix water towers state save

--- Project/Levels/Grand_garden/Worl_objects/Water_tower/Water_tower.gd
+++ Project/Levels/Grand_garden/Worl_objects/Water_tower/Water_tower.gd
@@ -5,11 +5,17 @@ signal cork_destroyed_on_water_tower(water_tawer_number)
 export(int) var water_tower_number
 
 onready var animationPlayer = $AnimationPlayer
+onready var audioPlayer = $Audio/WaterfallAudioPlayer
 onready var cork = $Cork
 
 func _ready():
 	cork.connect("cork_destroyed", self, "cork_destroyed") 
-	
+
+func setup_active():
+	cork.queue_free()
+	audioPlayer.play()
+	animationPlayer.play("Active_water")
+
 func cork_destroyed():
 	animationPlayer.play("Activate")
 	emit_signal("cork_destroyed_on_water_tower", water_tower_number)
