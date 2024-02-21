70d5110989b1042c4156e752ca6ac62e528a502e
Alexander <aa_rubtsov@mail.ru>
2024-02-21 18:59:36 +0300

    add icon animation

--- Project/UI/Components/GameHUD/GameHUD.gd
+++ Project/UI/Components/GameHUD/GameHUD.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 onready var label = $KissieCounter/HBoxContainer/Label
+onready var heart_icon_animation_player = $KissieCounter/HBoxContainer/HeartIcon/AnimationPlayer
 
 func _ready():
 	EventBus.connect("game_const_changed", self, "update_kissie_counter_ui")
@@ -15,3 +16,4 @@ func update_ui_data():
 func update_kissie_counter_ui(constant_name, value):
 	if constant_name == "kissies_count":
 		label.text = str(value)
+		heart_icon_animation_player.play("PickupImpact")
